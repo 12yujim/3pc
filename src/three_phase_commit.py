dd00a81c8f17d04eddcb6447eebd99e27cd4c933
Jim <jim.yu343@gmail.com>
2016-10-02 06:49:18 -0400

    initial coordinator setup, crashing

--- src/three_phase_commit.py
+++ src/three_phase_commit.py
@@ -16,21 +16,44 @@ PORT_BASE = 20000 # port_base
 
 class Client(object):
     def __init__(self, index, address, port):
+        global leader
         self.index = index
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.valid = True
         (clientsocket, address) = self.initialize_socket(port)
-        self.vote = 'yes'
+        leader = self.determineLeader() # initialize leader, start check at 0
+        if leader == self.index:
+            # notify master you are coordinator
+            self.send('coordinator ' + str(leader) + '\n')
+        self.vote = True
         self.crashAfterVote = False
         self.crashAfterAck = False
 
     def initialize_socket(self, port):
         self.sock.bind((address, port))
         self.sock.listen(5)
-        self.sock.accept()
+        return self.sock.accept()
+
+    def determineLeader(self):
+        global leader, address, PORT_BASE
+        for i in xrange(n):
+            try:
+                if i == self.index:
+                    continue
+                connectSock = socket(AF_INET, SOCK_STREAM)
+                connectSock.connect((address, PORT_BASE + i))
+                if self.valid:
+                    connectSock.send('info ' + str(self.index) + '\n')
+                ans = connectSock.recv(1024).split('\n')
+                leader = int(ans)
+                connectSock.close()
+            except:
+                continue
+        # did not connect to any process
+        return self.index
 
     def run(self):
-        global leader, n, PORT_BASE
+        global leader, n, address, PORT_BASE
         while self.valid:
             try:
                 # listen for input
@@ -40,33 +63,51 @@ class Client(object):
                     s = l.split()
                     if len(s) < 2:
                         continue
+                    if s[0] == 'info':
+                        # new process is asking for information, send leaderpid
+                        asker = s[1]
+                        sendSock = socket(AF_INET, SOCK_STREAM)
+                        sendSock.connect((address, PORT_BASE + asker))
+                        if self.valid:
+                            sendSock.send(str(leader) + '\n')
+                        sendSock.close()
                     if s[0] == 'add':
                         if leader == self.index:
                             # begin vote process
-                            pass
+                            if self.voteReq():
+                                self.send('resp commit\n')
+                                # write to file
+                            else:
+                                self.send('resp abort\n')
                         else:
                             continue
                     elif s[0] == 'delete':
                         if leader == self.index:
                             # begin vote process
-                            pass
+                            if self.voteReq():
+                                self.send('resp commit\n')
+                                # delete from file
+                            else:
+                                self.send('resp abort\n')
                         else:
                             continue
                     elif s[0] == 'get':
-                        # return the song (lookup in DT)
+                        # return the song (lookup in playlist)
                         pass
                     elif s[0] == 'crash':
                         #invoke crash
-                        pass
+                        self.close()
                     elif s[0] == 'vote':
                         if s[1] == 'NO':
-                            self.vote = 'no'
+                            self.vote = False
                         else:
                             continue
                     elif s[0] == 'crashAfterVote':
                         self.crashAfterVote = True
                     elif s[0] == 'crashAfterAck':
                         self.crashAfterAck = True
+                    elif s[0] == 'crashVoteREQ':
+                        pass
                     elif s[0] == 'crashPretialPreCommit':
                         pass
                     elif s[0] == 'crashPretialCommit':
@@ -74,6 +115,13 @@ class Client(object):
             except:
                 self.sock.close()
                 break
+
+    def voteReq(self):
+        # return True if all processes vote commit, else return False
+        if not self.vote:
+            # short circuit because coordinator votes no
+            return False
+        return True
 
     def send(self, s):
         if self.valid:
@@ -90,14 +138,9 @@ def main():
     global n, address
     index = int(sys.argv[1])
     n = int(sys.argv[2])
-    my_port = PORT_BASE + index
-    # assert(int(sys.argv[3]) == my_port)
+    my_port = int(sys.argv[3])
     client = Client(index, address, my_port)
-    while True:
-        try:
-            pass
-        except:
-            serversocket.close()
+    client.run()
 
 if __name__ == '__main__':
     main()
